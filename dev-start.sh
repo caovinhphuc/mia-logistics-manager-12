7485c91941b83689e3bfd53cf02401320cbabe8c
Cao Vĩnh Phúc <caovinhphuc.ios@gmail.com>
2025-12-25 17:05:23 +0700

    12

--- dev-start.sh
+++ dev-start.sh
@@ -2,7 +2,4 @@
 echo "🚀 Starting MIA Logistics Manager in development mode..."
 export NODE_ENV=development
 export BROWSER=none
-# Suppress Node.js deprecation warnings (util._extend is deprecated)
-# Only suppress deprecation warnings, keep other warnings
-export NODE_OPTIONS="--no-deprecation"
 npm start
